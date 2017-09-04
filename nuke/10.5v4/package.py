4cf4c70b6881c8eabc73f5a343e98c67f99019ff
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2017-09-04 16:04:09 +0200

    Add Tools and cleanup

--- nuke/10.5v4/package.py
+++ nuke/10.5v4/package.py
@@ -5,6 +5,7 @@ version_short = version.split('v')[0]
 authors = ["The Foundry"]
 description =  """ """
 build_requires = []
+requires = ['fixstudio']
 variants = [
     ["platform-linux", "arch-x86_64"]
 ]
@@ -13,29 +14,30 @@ uuid = "repository.nuke"
 
 has_plugins = True
 
+tools = ["Nuke"+str(version_short)]
+
 
 def commands():
-    #env.PATH.append("/prod/apps/nuke/{version}/linux")
     env.foundry_LICENSE = "4101@licfoundry.prs.vfx.int"
     env.FN_DISABLE_LICENSE_DIALOG = "1"
 
     import os
-    nuke_exe = os.path.join(
-        env.PP_SOFTWARE_APP.value(), #"/prod/apps/",
+    nuke_path = os.path.join(
+        env.PP_SOFTWARE_APP.value(),
         str(this.name),
         str(this.version),
-        env.PP_OS.value(),
-        "Nuke" + str(this.version_short))
+        env.PP_OS.value()
+    )
+    env.PATH.append(nuke_path)
+
+    nuke_exe = os.path.join(nuke_path, 'Nuke' + str(this.version_short))
 
     alias('nuke', nuke_exe)
+    alias('nukeassist', nuke_exe + ' -nukeassist')
+    alias('nukex', nuke_exe + ' -nukex')
+    alias('nukestudio', nuke_exe + ' -studio')
+
 
     if building:
-        #env.LD_LIBRARY_PATH.append("{root}/lib")
-        #env.Nuke_ROOT.append("/prod/apps/nuke/{version}/linux")
-        env.Nuke_ROOT.append(
-                os.path.join(
-                    env.PP_SOFTWARE_APP.value(),
-                    str(this.name),
-                    str(this.version),
-                    env.PP_OS.value()))
+        env.Nuke_ROOT.append(nuke_path)
 
