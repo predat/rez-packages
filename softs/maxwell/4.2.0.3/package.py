5cf6d3bc0e63b69f0f7b812ff0f9426ddd5a90d4
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-04-24 18:50:52 +0200

    maxwell: add version 4.2.0.3

---
+++ softs/maxwell/4.2.0.3/package.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) Fix Studio, and/or its licensors.
+# All rights reserved.
+#
+# The coded instructions, statements, computer programs, and/or related
+# material (collectively the "Data") in these files contain unpublished
+# information proprietary to Fix Studio and/or its licensors.
+#
+# The Data may not be disclosed or distributed to third parties or be
+# copied or duplicated, in whole or in part, without the prior written
+# consent of Fix Studio.
+
+from rez.utils.lint_helper import env, building  # make linter happy
+
+
+name = "maxwell"
+
+version = "4.2.0.3"
+
+authors = ["Next Limit"]
+
+description = ""
+
+variants = [["platform-linux", "arch-x86_64"]]
+
+build_requires = []
+
+requires = []
+
+tools = [
+    "maxwell",
+    "mxed",
+    "patchelf",
+    "mximerge",
+    "pymaxwell"
+    "studio"
+]
+
+uuid = "repository.maxwell"
+
+
+def commands():
+
+    env.MAXWELL_ROOT = "{root}"
+    env.MAXWELL4_ROOT = "{root}"
+    env.MAXWELL4_MATERIALS_DATABASE = "{root}/materials database"
+    env.MAXWELL_HONOR_UMASK = 1
+    env.nextlimit_LICENSE = "8053@licmaxwell.prs.vfx.int"
+
+    env.PATH.append("{root}")
+    env.LD_LIBRARY_PATH.append("{root}")
