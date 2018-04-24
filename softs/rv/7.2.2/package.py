d69a27e50794414daf1076237a0330076b76edee
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-04-24 18:47:23 +0200

    rv: add version 7.2.2

---
+++ softs/rv/7.2.2/package.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+from rez.utils.lint_helper import env
+
+
+name = "rv"
+
+version = "7.2.2"
+
+authors = ["RV"]
+
+description = "RV player and tools"
+
+requires = []
+
+variants = [["platform-linux", "arch-x86_64"]]
+
+tools = [
+    'rv',
+    'rvls',
+    'rvio'
+]
+
+uuid = "repository.rv"
+
+
+def commands():
+
+    env.PATH.append("{root}/bin")
