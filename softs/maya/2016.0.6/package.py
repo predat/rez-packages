de01608ed838c1cf77d0b994fa34face3b41d852
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-04-25 18:18:50 +0200

    maya: (2016) add MAYA_VERSION and Copyright

--- softs/maya/2016.0.6/package.py
+++ softs/maya/2016.0.6/package.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+
+# Copyright (C) Fix Studio, and/or its licensors.
+# All rights reserved.
+#
+# The coded instructions, statements, computer programs, and/or related
+# material (collectively the "Data") in these files contain unpublished
+# information proprietary to Fix Studio and/or its licensors.
+#
+# The Data may not be disclosed or distributed to third parties or be
+# copied or duplicated, in whole or in part, without the prior written
+# consent of Fix Studio.
+
+
 from rez.utils.lint_helper import env, building
 
 
@@ -24,7 +37,7 @@ has_plugins = True
 def commands():
 
     env.MAYA_LOCATION.set("{root}/maya")
-    env.MAYA_VERSION = "2016.5"
+    env.MAYA_VERSION = "2016"
 
     env.AUTODESK_ADLM_THINCLIENT_ENV.set("{root}/AdlmThinClientCustomEnv.xml")
     env.MAYA_COLOR_MANAGEMENT_POLICY_LOCK = 1
