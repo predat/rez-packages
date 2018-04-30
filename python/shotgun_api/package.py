2fba9fc61176778f937f5912348091d693be7d87
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-04-30 16:19:53 +0200

    Add shotgun-api

---
+++ python/shotgun_api/package.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) Fix Studio, and/or its licensors.
+# All rights reserved.
+#
+# The coded instructions, statements, computer programs, and/or related
+# material (collectively the "Data") in these files contain unpublished
+# information proprietary to Fix Studio and/or its licensors.
+#
+# The Data may not be disclosed or distributed to third parties or be
+# copied or duplicated, in whole or in part, without the prior written
+# consent of Fix Studio.
+
+from rez.utils.lint_helper import env, building
+
+
+name = "shotgunapi"
+
+version = "3.0.34"
+
+authors = ["Shotgun Software"]
+
+description = "Python-based API for accessing Shotgun"
+
+variants = [["platform-linux", "arch-x86_64", "python-2.7"]]
+
+tools = []
+
+build_requires = ['setuptools']
+
+uuid = "repository.shotgunapi"
+
+
+def commands():
+    env.PYTHONPATH.append("{root}/python")
