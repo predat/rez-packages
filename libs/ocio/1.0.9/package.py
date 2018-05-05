40ebf7c8e866d8e1120e383643d0238ea0516a5e
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-05-05 14:42:21 +0200

    ocio: cleanup

--- libs/ocio/1.0.9/package.py
+++ libs/ocio/1.0.9/package.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from rez.utils.lint_helper import env, building, scope  # make linter happy
+
+
 name = "ocio"
 
 version = "1.0.9"
@@ -9,43 +14,24 @@ description = \
     """
     """
 
-#build_requires = [
-#    "python-2.7",
-#    "glew-1",
-#    "ilmbase-2.2.0",
-#    "openexr-2.2.0",
-#    "oiio",
-#]
-
-requires = [
-     "python"
-#    "oiio",
-#    "glew-1",
-#    "boost-1.63",
-#    "ilmbase-2.2.0",
-#    "openexr-2.2.0",
-]
-
 variants = [
-    ["platform-linux", "arch-x86_64"]
+    ["platform-linux", "arch-x86_64", "python-2.7"]
 ]
 
 tools = [
     "ociobakelut",
     "ociocheck",
-    #"ocioconvert",
-    #"ociodisplay",
-    #"ociolutimage"
+    "ocioconvert",
+    "ociodisplay",
+    "ociolutimage"
 ]
-
 
 uuid = "repository.ocio"
 
+
 def commands():
-    env.CMAKE_MODULE_PATH.append("{root}/cmake")
-    env.PATH.append("{root}/bin")
     env.LD_LIBRARY_PATH.append("{root}/lib")
     env.PYTHONPATH.append("{root}/lib/python2.7/site-packages")
 
     if building:
-        env.OCIO_INCLUDE_DIR = "{root}/include"
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
