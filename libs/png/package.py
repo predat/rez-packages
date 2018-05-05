1b506d890c55c1fac144cfd80efcf9dc2287cbd0
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-05-05 14:32:02 +0200

    png: add png version 1.6.29

---
+++ libs/png/package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from rez.utils.lint_helper import env, building, scope  # make linter happy
+
+
+name = 'png'
+
+version = '1.6.29'
+
+authors = ['fredrik.brannbacka']
+
+variants = [["platform-linux", "arch-x86_64"]]
+
+
+def commands():
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
