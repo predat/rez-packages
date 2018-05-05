516c3038332f91c157185ba96544f8006059d396
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-05-05 14:14:33 +0200

    glew: add version 2.0.0

---
+++ libs/glew/2.0.0/package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from rez.utils.lint_helper import env, building, scope  # make linter happy
+
+
+name = 'glew'
+
+version = '2.0.0'
+
+authors = ['fredrik.brannbacka']
+
+variants = [["platform-linux", "arch-x86_64"]]
+
+
+def commands():
+    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
+
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
