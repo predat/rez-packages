7a7572948faee70f3a4a7cf4bf81c889c012a0ed
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-05-07 17:57:44 +0200

    ffmpeg: set specific git tag...

--- libs/openexr/2.2.0/cmake/openexr.cmake
+++
@@ -1,6 +0,0 @@
-find_package(PkgConfig)
-set( ENV{PKG_CONFIG_PATH} $ENV{PKG_CONFIG_PATH}:$ENV{REZ_OPENEXR_ROOT}/lib/pkgconfig)
-pkg_check_modules (OPENEXR REQUIRED OpenEXR)
-set(openexr_INCLUDE_DIRS    ${OPENEXR_INCLUDE_DIRS})
-set(openexr_LIBRARY_DIRS    ${OPENEXR_LIBRARY_DIRS})
-set(openexr_LIBRARIES ${OPENEXR_LIBRARIES})
