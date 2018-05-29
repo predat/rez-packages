a4703225deab97f503a6024788b704a3ca4aa9b8
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-05-29 12:14:55 +0200

    muster: add v9.0.4

---
+++ softs/muster/9.0.4/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BINARY_DIR=$1
+PREFIX_DIR=$2
+# MAJOR_VERSION=$3
+
+echo "Installing Muster files."
+for file in ${BINARY_DIR}/muster/src/muster/*
+do
+    echo "Installing: ${file}"
+    cp -arf "${file}" "$PREFIX_DIR"
+done
+
+cd $PREFIX_DIR
+chmod 755 plugins scripts templates htdocs extra python python/Lib
+chmod -R 777 templates
+
