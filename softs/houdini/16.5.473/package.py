052463222a53246d69e1414fdb0d9a4b8e021e93
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-07 14:43:50 +0200

    houdini: add version 16.5.473

---
+++ softs/houdini/16.5.473/package.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from rez.utils.lint_helper import env, building, source, command
+
+
+name = "houdini"
+
+version = "16.5.473"
+
+authors = ["SideFx"]
+
+description = "Houdini version %s" % version
+
+variants = [
+    ["platform-linux", "arch-x86_64"]
+]
+
+tools = [
+    'houdini',
+    'houdinifx',
+    'mview',
+    'mplay',
+    'abcconvert',
+    'abcinfo',
+    'hbatch',
+    'hcommand',
+    'hcompile',
+    'hserver',
+    'hview',
+    'hython',
+    'mantra'
+]
+
+has_plugins = True
+
+uuid = "repository.houdini"
+
+
+def commands():
+
+    command("rm -rf ~/.sesi_licenses.pref")
+
+    env.SESI_LMHOST = "lichoudini.prs.vfx.int"
+
+    source("{root}/houdini/houdini_setup_bash")
+
+    env.PATH.prepend("{root}/houdini/bin")
+
+    # if building:
+    #     env.CMAKE_MODULE_PATH.append("{root}/cmake")
