3a11a10ca777fa81b4c77103c9773bc3d7b762c7
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-15 10:19:35 +0200

    tbb: fix typo in cmake file

--- libs/tbb/4.4.6/cmake/tbb.cmake
+++ libs/tbb/4.4.6/cmake/tbb.cmake
@@ -2,6 +2,6 @@ if(NOT tbb_COMPONENTS)
     set(tbb_COMPONENTS tbb)
 endif(NOT tbb_COMPONENTS)
 
-set(tbb_INCLUDE_DIRS       $ENV{REZ_TBB_ROOT}/tbb/include)
-set(tbb_LIBRARY_DIRS       $ENV{REZ_TBB_ROOT}/tbb/lib)
+set(tbb_INCLUDE_DIRS       $ENV{REZ_TBB_ROOT}/include)
+set(tbb_LIBRARY_DIRS       $ENV{REZ_TBB_ROOT}/lib)
 set(tbb_LIBRARIES          ${tbb_COMPONENTS})
