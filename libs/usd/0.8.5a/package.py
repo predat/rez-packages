6917b66583aa278c5ccd62691ed52eee742145d1
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-15 16:20:33 +0200

    usd: add version 0.8.5a

---
+++ libs/usd/0.8.5a/package.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from rez.utils.lint_helper import env, building, scope, early, resolve  # make linter happy
+
+
+name = "usd"
+
+version = "0.8.5a"
+
+authors = ['Pixar']
+
+description = \
+    """
+Universal Scene Description (USD) is an efficient, scalable system for authoring, reading, and streaming time-sampled scene description for interchange between graphics applications.
+    """
+
+variants = [
+    ["platform-linux", "arch-x86_64", "python-2.7"]
+]
+
+
+build_requires = [
+    'alembic',
+    'hdf5',
+    'boost',
+    'tbb',
+    'openexr',
+    'oiio',
+    'glew',
+    'opensubdiv',
+    'Jinja2',
+    'oiio',
+    'opensubdiv',
+    'ptex',
+    'maya'
+]
+
+requires = []
+
+tools = []
+
+uuid = "repository.usd"
+
+
+def commands():
+    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.PYTHONPATH.append("{root}/lib/python")
+    env.PATH.append("{root}/bin")
+
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
+
+    if "maya" in resolve:
+        env.MAYA_PLUG_IN_PATH.append("{root}/third_party/maya/plugin")
+        env.MAYA_SCRIPT_PATH.append("{root}/third_party/maya/share/usd/plugins/usdMaya/resources")
+        env.XBMLANGPATH.append("{root}/third_party/maya/share/usd/plugins/usdMaya/resources/%B")
