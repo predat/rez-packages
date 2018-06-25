3a9018ae445041d4950e1be19299c81b8b594220
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-25 15:53:53 +0200

    tiff: fix libraries path

--- libs/tiff/package.py
+++ libs/tiff/package.py
@@ -34,7 +34,7 @@ uuid = 'repository.tiff'
 
 def commands():
     env.PATH.prepend("{root}/bin")
-    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib64")
 
     if building:
         env.CMAKE_MODULE_PATH.append("{root}/cmake")
