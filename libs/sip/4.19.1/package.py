307838dffa0aaea9bafdfaf4a1c1cf948e7c4d5d
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-25 11:48:52 +0200

    Add sip

---
+++ libs/sip/4.19.1/package.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from rez.utils.lint_helper import env, building, scope, early, resolve  # make linter happy
+
+
+name = "sip"
+
+version = "4.19.1"
+
+authors = ['']
+
+description = \
+    """
+    """
+
+variants = [
+    ["platform-linux", "arch-x86_64", "python-2.7"]
+]
+
+
+build_requires = [
+]
+
+requires = [
+    'python-2.7'
+]
+
+tools = []
+
+uuid = "repository.sip"
+
+
+def commands():
+    env.PYTHONPATH.append("{root}/lib/python2.7/site-packages")
+    env.PATH.append("{root}/bin")
