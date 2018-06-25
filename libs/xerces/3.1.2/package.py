34f3f5bcdb872d1ea5c5258f3acff2aee141f292
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-25 13:57:04 +0200

    add xerces version 3.1.2

---
+++ libs/xerces/3.1.2/package.py
@@ -0,0 +1,27 @@
+name = "xerces"
+
+version = "3.1.2"
+
+authors = [
+]
+
+description = \
+    """
+Xerces-C++ is a validating XML parser written in a portable subset of C++. Xerces-C++ makes it easy to give your application the ability to read and write XML data. A shared library is provided for parsing, generating, manipulating, and validating XML documents using the DOM, SAX, and SAX2 APIs.
+    """
+
+build_requires = []
+
+variants = [
+    ["platform-linux", "arch-x86_64"]
+]
+
+tools = []
+
+uuid = "repository.xerces"
+
+
+def commands():
+
+    if building:
+        env.LD_LIBRARY_PATH.append("{root}/lib")
