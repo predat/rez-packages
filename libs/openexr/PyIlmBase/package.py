36ea6da775c233f1e5ffea032e0150ff01a13bc1
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-06-29 18:04:19 +0200

    openexr: move PyIlmBase into a specific package

--- libs/openexr/PyIlmBase/package.py
+++ libs/openexr/PyIlmBase/package.py
@@ -2,32 +2,33 @@
 from rez.utils.lint_helper import env, building
 
 
-name = "openexr"
+name = "PyIlmBase"
 
 version = "2.2.0"
 
 description = \
     """
-    OpenEXR
     """
 
 build_requires = [
-    # 'cmake-3',
+    #'cmake-3',
 ]
 
 requires = [
-    # 'boost-1.61',
-    #'numpy'
+    'openexr-2.2',
+    'boost-1.61',
+    'numpy'
 ]
 
-variants = [["platform-linux", "arch-x86_64"]]
+variants = [["platform-linux", "arch-x86_64", "python-2.7"]]
 
-uuid = "repository.openexr"
+uuid = "repository.PyIlmBase"
 
 
 def commands():
     env.PATH.prepend("{root}/bin")
     env.LD_LIBRARY_PATH.prepend("{root}/lib")
+    env.PYTHONPATH.append("{root}/lib64/python2.7/site-packages")
 
     if building:
         env.CMAKE_MODULE_PATH.append("{root}/cmake")
