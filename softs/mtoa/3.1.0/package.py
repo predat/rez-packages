0e982b1105aeb6bca85af514479ec881aba22b12
Sylvain Maziere <sylvain.maziere@fixstudio.com>
2018-08-23 17:12:38 +0200

    mtoa: bump version to 3.1.0

---
+++ softs/mtoa/3.1.0/package.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from rez.utils.lint_helper import env, building
+
+
+name = "mtoa"
+
+version = "3.1.0"
+
+authors = ["SolidAngle"]
+
+description = ""
+
+variants = [
+    ["platform-linux", "arch-x86_64", "maya-2016"],
+    ["platform-linux", "arch-x86_64", "maya-2016.5"],
+    ["platform-linux", "arch-x86_64", "maya-2017"],
+    ["platform-linux", "arch-x86_64", "maya-2018"]
+]
+
+plugin_for = ["maya"]
+
+tools = [
+    'kick',
+    'oslc',
+    'oslinfo',
+    'maketx'
+]
+
+build_command = "bash {root}/build.sh {install}"
+
+uuid = "repository.mtoa"
+
+
+def commands():
+
+    env.PATH.append("{root}/bin")
+    env.LD_LIBRARY_PATH.append("{root}/bin")
+    env.MAYA_MODULE_PATH.append("{root}")
+    env.MAYA_RENDER_DESC_PATH.append("{root}")
+    env.MTOA_EXTENSIONS_PATH.append("{root}/extensions")
+    env.solidangle_LICENSE = "5053@licarnold.prs.vfx.int"
