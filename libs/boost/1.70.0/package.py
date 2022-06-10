fe38a6a8c7bddc9a2183f03533b2422b5e050109
Linas Beresna <beres.linas@gmail.com>
2022-06-09 18:35:59 -0700

    Updated boost to have variants for python3 and python2

--- libs/boost/1.70.0/package.py
+++ libs/boost/1.70.0/package.py
@@ -3,28 +3,42 @@ name = "boost"
 version = "1.70.0"
 
 authors = [
-    "boost.org"
+    "Beman Dawes",
+    "David Abrahams"
 ]
 
 description = \
     """
-    Peer-reviewed portable C++ source libraries.
+    Boost is a set of libraries for the C++ programming language that provide support for tasks and structures such
+    as linear algebra, pseudorandom number generation, multithreading, image processing, regular expressions,
+    and unit testing.
     """
 
-private_build_requires = [
-    "gcc-6+"
+requires = [
+    "platform-linux",
+    "cmake-3+",
+    "gcc-6+",
 ]
 
 variants = [
-    ["platform-linux", "python-2.7"],
-    ["platform-linux", "python-3.6"],
+    ["python-2.7"],
+    ["python-3.6"]
 ]
 
-uuid = "repository.boost"
+tools = [
+    "boost"
+]
+
+build_system = "cmake"
+
+with scope("config") as config:
+    config.build_thread_count = "logical_cores"
+
+uuid = "boost-{version}".format(version=str(version))
 
 def commands():
-    env.LD_LIBRARY_PATH.append("{root}/lib")
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
 
-    # cmake FindPackage env vars
-    if building:
-        env.CMAKE_PREFIX_PATH.append("{root}")
+    # Helper environment variables.
+    env.BOOST_INCLUDE_PATH.set("{root}/include")
+    env.BOOST_LIBRARY_PATH.set("{root}/lib")
