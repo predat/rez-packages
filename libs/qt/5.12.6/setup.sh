5b58fdba0071830f3bf32ce663c3fe68a0de5177
Linas Beresna <beres.linas@gmail.com>
2022-06-09 19:46:35 -0700

    First try for qt but things going wrong

---
+++ libs/qt/5.12.6/setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mkdir -p ${REZ_REPO_PAYLOAD_DIR}/qt/
+wget -O ${REZ_REPO_PAYLOAD_DIR}/qt/qt-everywhere-src-5.12.6.tar.xz http://download.qt.io/official_releases/qt/5.12/5.12.6/single/qt-everywhere-src-5.12.6.tar.xz
