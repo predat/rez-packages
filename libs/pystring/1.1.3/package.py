153429a8998f34028d2ff83c96b88c31bd94ab8a
Linas Beresna <beres.linas@gmail.com>
2022-08-30 14:59:36 -0700

    Added pystring 1.1.3

---
+++ libs/pystring/1.1.3/package.py
@@ -0,0 +1,35 @@
+# Taken from OSS-Pipeline from https://github.com/OSS-Pipeline/rez-ilmbase
+
+name = "pystring"
+
+version = "1.1.3"
+
+authors = [
+    "Sony Picture Imageworks"
+]
+
+description = \
+    """
+    C++ functions matching the interface and behavior of python string methods with std::string
+    """
+
+requires = [
+    "cmake-3.12+",
+    "gcc-6+"
+]
+
+variants = [
+    ["platform-linux"]
+]
+
+build_system = "cmake"
+
+with scope("config") as config:
+    config.build_thread_count = "logical_cores"
+
+uuid = "pystring-{version}".format(version=str(version))
+
+def commands():
+    env.LD_LIBRARY_PATH.prepend("{root}/lib")
+
+    env.PYSTRING_LIBRARY_PATH.set("{root}/lib")
---
+++ libs/pystring/1.1.3/setup.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+mkdir -p ${REZ_REPO_PAYLOAD_DIR}/pystring/
+wget -O ${REZ_REPO_PAYLOAD_DIR}/pystring/pystring-1.1.3.zip https://github.com/imageworks/pystring/archive/refs/heads/master.zip
