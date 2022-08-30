620fecc94323cbf7abf3dd94e5dfae07f9c449e9
Linas Beresna <beres.linas@gmail.com>
2022-08-30 16:56:22 -0700

    lil cleanup

--- libs/tbb/4.4/cmake/tbb.cmake
+++
@@ -1,7 +0,0 @@
-if(NOT tbb_COMPONENTS)
-    set(tbb_COMPONENTS tbb)
-endif(NOT tbb_COMPONENTS)
-
-set(tbb_INCLUDE_DIRS       $ENV{REZ_TBB_ROOT}/tbb/include)
-set(tbb_LIBRARY_DIRS       $ENV{REZ_TBB_ROOT}/tbb/lib)
-set(tbb_LIBRARIES          ${tbb_COMPONENTS})
