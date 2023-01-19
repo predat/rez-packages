4cd8bd9794bbcddfb5c7860b68f10fd452a79f9f
predat <sylvain@predat.fr>
2023-01-19 12:23:34 +0100

    Bump up swig version to 4.11.1

--- libs/swig/package.py
+++ libs/swig/package.py
@@ -2,25 +2,25 @@
 
 name = 'swig'
 
-version = '4.0.1'
+version = '4.1.1'
 
 description = """
-SWIG is a software development tool that connects programs written in C and C++ with a variety of high-level programming languages.
+SWIG is a software development tool that connects programs written in C and C++
+with a variety of high-level programming languages.
 """
-
-authors = ['']
 
 variants = [
     ['platform-linux', 'python-2.7'],
-    ['platform-linux', 'python-3.7']
+    ['platform-linux', 'python-3.7'],
+    ['platform-linux', 'python-3.9']
 ]
 
 requires = []
 
 private_build_requires = [
-    'gcc-6.3.1',
+    'gcc-9+',
     'cmake-3+',
-    'boost-1.70',
+    'boost-1.80',
 ]
 
 uuid = 'repository.%s' % name
